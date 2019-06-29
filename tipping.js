d256e159b56bd5711f08078b3a561a8f6460fe18
Danny Ward <liege88@gmail.com>
2019-06-29 22:21:41 +0800

    create new repository

---
+++ tipping.js
@@ -0,0 +1,20 @@
+// Find PI to the Nth Digit -
+// Enter a number and have the program generate PI up to that many decimal places.
+// Keep a limit to how far the program will go.
+//
+// const pi = [1,4,1,5,9,2,6,5,3,5,8,9,7,9,3];
+// let decimal = 0;
+// 
+// userValue = prompt("Display Pi to __ decimal places");
+// userValue = parseInt(userValue)
+// pi.length = userValue;
+// endString = "3.";
+//
+// if (userValue > 15 || userValue < 2) {
+//   alert("This program can only generate Pi between 2 and 15 decimal places.");
+// } else {
+//   for (let i = 0; i <= (pi.length - 1); i += 1) {
+//     endString += pi[i];
+//   }
+//   alert("Pi to " + userValue + " decimal places: " + endString);
+// }
